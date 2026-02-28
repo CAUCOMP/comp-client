d6a7b32502e47fccca4209df64a2a57d08b0be98
Kim Seungbeom <tmdqja3201@gmail.com>
2026-03-01 01:17:23 +0900

    헤더 네비 바 초안 코드 작성

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,12 @@
+ import "./Nav.css"
+
+ <header className="nav">
+    <div className="navLeft">
+        <div className="brandMark" aria-hidden />
+          <nav className="navLinks" aria-label="Top navigation">
+            <a href="#">아카이브</a>
+            <a href="#">프로젝트</a>
+            <a href="#">지원하기</a>
+          </nav>
+        </div>
+</header>
