0dedff196975af5df27c414e9c04c4d96c751255
Kim Seungbeom <tmdqja3201@gmail.com>
2026-03-02 09:59:15 +0900

    refactor/Navbar.jsx export 누락으로 인한 수정

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,13 +1,16 @@
-import "./Nav.css"
-import { Link } from "react-router-dom"
+import "./Navbar.css";
 
- <header className="nav">
-    <div className="navLeft">
-        <div className="brandMark" aria-hidden />
-          <nav className="navLinks" aria-label="Top navigation">
-            <Link to="#">아카이브</Link>
-            <Link to="#">프로젝트</Link>
-            <Link to="#">지원하기</Link>
-          </nav>
-        </div>
-</header>
+export default function Navbar() {
+  return (
+    <header className="nav">
+      <div className="navLeft">
+        <div className="brandMark" aria-hidden="true" />
+        <nav className="navLinks" aria-label="Top navigation">
+          <a href="#">아카이브</a>
+          <a href="#">프로젝트</a>
+          <a href="#">지원하기</a>
+        </nav>
+      </div>
+    </header>
+  );
+}
