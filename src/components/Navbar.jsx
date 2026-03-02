2420d6dfaa4578ec09a3c473f53cf6560a5224a5
Kim Seungbeom <tmdqja3201@gmail.com>
2026-03-02 09:49:20 +0900

    feat/a -> link로 수정

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,12 +1,13 @@
- import "./Nav.css"
+import "./Nav.css"
+import { Link } from "react-router-dom"
 
  <header className="nav">
     <div className="navLeft">
         <div className="brandMark" aria-hidden />
           <nav className="navLinks" aria-label="Top navigation">
-            <a href="#">아카이브</a>
-            <a href="#">프로젝트</a>
-            <a href="#">지원하기</a>
+            <Link to="#">아카이브</Link>
+            <Link to="#">프로젝트</Link>
+            <Link to="#">지원하기</Link>
           </nav>
         </div>
 </header>
