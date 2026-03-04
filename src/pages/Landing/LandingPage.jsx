2bcd880d8fc281b69165a2aa277c27e8956d5ba1
Jihaeee <elinkang02@gmail.com>
2026-03-04 14:19:58 +0900

    feat: Hero 섹션 UI 구현

---
+++ src/pages/Landing/LandingPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import HeroSection from './sections/HeroSection';
+
+const LandingPage = () => {
+  return (
+    <>
+    <HeroSection />
+    </>
+  )
+}
+
+export default LandingPage;
