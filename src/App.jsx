6eb7646784e1cd425ec1a1674d4545330fd64780
Jihaeee <elinkang02@gmail.com>
2026-03-05 17:36:35 +0900

    feat: 로그인 페이지 라우트 추가

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,18 @@
-import MainLayout from "@/layout/MainLayout"
+import { Routes, Route } from "react-router-dom";
+import MainLayout from "@/layout/MainLayout";
 import LandingPage from "@/pages/Landing/LandingPage";
+import LoginPage from "./pages/LoginPage";
 
 function App() {
-
   return (
-    <MainLayout>
-      <LandingPage />
-    </MainLayout>
-  )
+    <Routes>
+      <Route element={<MainLayout />}>
+        <Route path="/" element={<LandingPage />} />
+      </Route>
+
+      <Route path="/login" element={<LoginPage />} />
+    </Routes>
+  );
 }
 
 export default App;
--- src/layout/MainLayout.jsx
+++ src/layout/MainLayout.jsx
@@ -1,11 +1,12 @@
 import Navbar from "@/components/Navbar"
+import { Outlet } from "react-router-dom";
 
 const MainLayout = ({ children }) => {
   return (
     <div className="flex flex-col min-h-screen">
       <Navbar />
       <main className="pt-5">
-        {children}
+        <Outlet />
       </main>
     </div>
   )
