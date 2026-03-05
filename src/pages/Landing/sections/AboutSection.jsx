db225fa5bd10ce2dc345194cb2cc0f8dc4b2404c
Jihaeee <elinkang02@gmail.com>
2026-03-06 01:29:28 +0900

    feat: 메인 페이지 소개 섹션 레이아웃 구현 (#12)

--- src/pages/Landing/sections/AboutSection.jsx
+++ src/pages/Landing/sections/AboutSection.jsx
@@ -1,8 +1,25 @@
+import ImageSlider from '@/components/ImageSlider'
 import React from 'react'
 
 const AboutSection = () => {
   return (
-    <div>AboutSection</div>
+    <section className="min-h-screen flex flex-col justify-center p-10">
+        <div>
+            <div>Why COMP?</div>
+            <div>컴퓨터와 나와 친구들이 있는 곳</div>
+        </div>
+        {/* 컨텐츠 영역 */}
+        <div className="flex gap-5">
+            <ImageSlider />
+            <div>
+                <p>스터디</p>
+                <p>특별세션</p>
+                <p>실전 프로젝트</p>
+                <p>콤프인의 밤</p>
+            </div>
+        </div>
+
+    </section>
   )
 }
 
