7c457e5528ebfb2d7cb439de303f8ec55563c9e1
Jihaeee <elinkang02@gmail.com>
2026-03-07 15:21:47 +0900

    feat: 출석 체크 및 스터디 과제 제출 페이지 UI 구현

--- src/pages/Attendance/StudySubmitPage.jsx
+++ src/pages/Attendance/StudySubmitPage.jsx
@@ -1,8 +1,25 @@
-import React from 'react'
+import { useState } from 'react'
+import AttendanceForm from './components/AttendanceForm'
 
 const StudySubmitPage = () => {
+    const [file, setFile] = useState(null)
+    const [submitted, setSubmitted] = useState(false)  
+
+  const handleSubmit = () => {
+    // api 연결 후 제출 성공하면 
+    setSubmitted(true)
+  }
+
   return (
-    <div>StudySubmitPage</div>
+    <AttendanceForm
+      title="이번주 스터디 과제를 제출해주세요."
+      subTitle={submitted ? "과제 제출이 완료되었어요!" : "아직 제출 전이에요!"}
+      file={file}
+      onFileChange={(e) => setFile(e.target.files[0])}
+      fileText="과제 제출하기"
+      onSubmit={handleSubmit}
+      buttonText="제출하기"
+    />
   )
 }
 
