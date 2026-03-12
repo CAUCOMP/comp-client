1933c047725197a944ccbbcec79978a070e87227
Jihaeee <elinkang02@gmail.com>
2026-03-12 20:35:42 +0900

    revert merge

--- src/pages/landing/LandingPage.jsx
+++ src/pages/landing/LandingPage.jsx
@@ -1,27 +1,13 @@
 import ScrollBar from "@/components/ScrollBar";
 import HeroSection from "./sections/HeroSection";
 import AboutSection from "./sections/AboutSection";
-import { useState, useRef } from "react";
+import { useState } from "react";
 import FAQSection from "./sections/FAQSection";
 
 const TOTAL = 3;
 
 const LandingPage = () => {
   const [current, setCurrent] = useState(0)
-  const isScrolling = useRef(false) // 휠 이동 한번에 너무 많이 되는거 방지용 변수 
-
-  const handleWheel = (e) => {
-  if (isScrolling.current) return;
-
-  isScrolling.current = true;
-
-  if (e.deltaY > 0) handleNext();
-  else handlePrev();
-
-  setTimeout(() => {
-    isScrolling.current = false;
-  }, 800); //0.8초간 잠금
-}; //새로만든 함수
 
   const handleNext = () => {
     if (current < TOTAL -1) setCurrent(prev => prev+1)
@@ -33,10 +19,9 @@ const LandingPage = () => {
   
   return (
     <div 
-      className="h-screen overflow-hidden"
+      className="h-screen"
       // 마우스휠 감지해서 휠로도 위아래 섹션 이동 가능 (마우스 움직이는 만큼 스크롤할건지 섹션이동시킬건지)
-      //onWheel={(e) => e.deltaY > 0 ? handleNext() : handlePrev()}
-      onWheel={handleWheel}
+      onWheel={(e) => e.deltaY > 0 ? handleNext() : handlePrev()}
       >
         <div
           className="transition-transform duration-700 ease-in-out"
