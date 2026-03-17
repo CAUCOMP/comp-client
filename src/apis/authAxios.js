e59164b1a82b1557e3f180baf73989fff76386fc
Jihaeee <elinkang02@gmail.com>
2026-03-17 09:46:16 +0900

    refactor: authAxios에서 getNewRefreshToken, logout 사용하도록 변경

--- src/apis/authAxios.js
+++ src/apis/authAxios.js
@@ -1,4 +1,5 @@
 import axios from "axios"; // HTTP 요청을 보내는 라이브러리
+import { getNewRefreshToken, logout } from "./auth";
 
 const authAxios = axios.create({
   baseURL: "http://3.38.112.62:8080",
@@ -24,20 +25,13 @@ authAxios.interceptors.response.use(
       
       try {
         // 토큰 갱신
-        const { data } = await axios.post("/api/auth/refresh", {
-          refreshToken: localStorage.getItem("refreshToken"),
-        });
-
-        // 새 토큰 저장
-        localStorage.setItem("accessToken", data.accessToken);
+        await getNewRefreshToken();
         // 기존 요청에 새 토큰 적용
-        error.config.headers.Authorization = `Bearer ${data.accessToken}`;
+        error.config.headers.Authorization = `Bearer ${localStorage.getItem('accessToken')}`;
         
         return authAxios(error.config); // 재시도
       } catch {
-        // 갱신 실패시 로그아웃
-        localStorage.clear();
-        window.location.href = "/login";
+        logout();
       }
     }
     return Promise.reject(error);
